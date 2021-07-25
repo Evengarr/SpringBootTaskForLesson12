b82b97a6bad0ce89848d055392ad761d4b548a66
Evengarr <Ravasect@gmail.ru>
2021-07-25 18:58:34 +0300

    Initial commit

---
+++ src/test/java/com/example/springboottaskforlesson12/SpringBootTaskForLesson12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboottaskforlesson12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTaskForLesson12ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
